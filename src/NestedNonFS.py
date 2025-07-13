ecfd5115710d988d1736126fdf60169ed6a365fa
Ignacio Garach <44344214+nach00gar@users.noreply.github.com>
2025-07-13 17:54:51 +0200

    Add files via upload

--- src/NestedNonFS.py
+++ src/NestedNonFS.py
@@ -48,18 +48,86 @@ NONFS_GRID = {
 
 }
 
-def run_nested_cv_experiment_avg(datasets, normalization="TSS"):
+from scipy.special import comb
+
+def rrarefy_df(df, sample_size):
+    df = df.round().astype(int)
+
+    if isinstance(sample_size, (int, float)):
+        sample_size = np.full(df.shape[0], sample_size)
+    elif isinstance(sample_size, (list, np.ndarray, pd.Series)):
+        sample_size = np.asarray(sample_size)
+        if len(sample_size) != df.shape[0]:
+            raise ValueError("'sample_size' length does not match")
+    else:
+        raise ValueError("sample_size must be a number or an iterable of the same length as the number of samples")
+
+    rarefied = []
+    np.random.seed(42)
+    for i, row in enumerate(df.values):
+        total = row.sum()
+        if total < sample_size[i]:
+            rarefied.append(row)  
+        else:
+            labels = np.repeat(np.arange(len(row)), row)
+            subsample = np.random.choice(labels, size=sample_size[i], replace=False)
+            new_counts = np.bincount(subsample, minlength=len(row))
+            rarefied.append(new_counts)
+
+    return pd.DataFrame(rarefied, columns=df.columns, index=df.index)
+
+def rrarefy_min_quantile(df, quantile=0.05, labels=None, random_state=42):
+    if random_state is not None:
+        np.random.seed(random_state)
+
+    df = df.round().astype(int)
+
+    total_reads = df.sum(axis=1)
+    min_depth = np.floor(np.quantile(total_reads, quantile)).astype(int)
+    mask = total_reads >= min_depth
+    df_filtered = df[mask]
+    if df_filtered.empty:
+        raise ValueError("No samples remain after applying the minimum depth filter.")
+
+    rarefied = []
+    for row in df_filtered.values:
+        tags  = np.repeat(np.arange(len(row)), row)
+        subsample = np.random.choice(tags, size=min_depth, replace=False)
+        new_counts = np.bincount(subsample, minlength=len(row))
+        rarefied.append(new_counts)
+
+    rarefied_df = pd.DataFrame(rarefied, columns=df.columns, index=df_filtered.index)
+
+    if labels is not None:
+        if isinstance(labels, (pd.Series, np.ndarray, list)):
+            labels = pd.Series(labels, index=df.index) if not isinstance(labels, pd.Series) else labels
+            labels_filtered = labels[mask]
+            return rarefied_df, labels_filtered
+        else:
+            raise ValueError("'labels' must be pd.Series, np.ndarray o list.")
+
+    return rarefied_df
+
+def run_nested_cv_experiment_avg(datasets, normalization="TSS", rarefaction= "NO"):
     results = pd.DataFrame(columns=['Dataset', 'Model', 'Mean AUC', 'Std AUC', 'Mean Accuracy', 'Std Accuracy'])
     
     for dataset in datasets:
-        df = pd.read_csv("../data/" + dataset + ".csv")
+        df = pd.read_csv("./data/" + dataset + ".csv")
         y = df["label"]
         df.drop("label", axis=1, inplace=True)
+        if rarefaction == "MIN":
+            df = rrarefy_df(df, df.sum(axis=1).min())
+        if rarefaction == "Q05":
+            df, y = rrarefy_min_quantile(df, 0.05, y)
         
         if normalization == "TSS":
             df = pd.DataFrame(closure(df))
         if normalization == "CLR":
             df = pd.DataFrame(clr(closure(df + 0.5)))
+        if normalization == "logTSS":
+            df = pd.DataFrame(np.log10(closure(df + 0.5)))
+        if normalization == "PA":
+            df = (df>0).astype(int)
             
         label_encoder = LabelEncoder()
         y = label_encoder.fit_transform(y.squeeze())
@@ -88,6 +156,9 @@ def run_nested_cv_experiment_avg(datasets, normalization="TSS"):
             std_auc = np.std(auc_scores)
             mean_accuracy = np.mean(accuracy_scores)
             std_accuracy = np.std(accuracy_scores)
+
+            print(dataset)
+            print(mean_auc)
             
             results.loc[len(results)] = [
                 dataset, 
@@ -100,7 +171,12 @@ def run_nested_cv_experiment_avg(datasets, normalization="TSS"):
     
     return results
 
-tss = run_nested_cv_experiment_avg(datasets, normalization="TSS")
-clr = run_nested_cv_experiment_avg(datasets, normalization="CLR")
-tss.to_csv("NestedTSSResults.csv")
-clr.to_csv("NestedCLRResults.csv")
+normalizations = ["TSS", "CLR", "logTSS", "PA"]
+rarefactions = ["NO", "MIN", "Q05"]
+
+for norm in normalizations:
+    for rare in rarefactions:
+        print(f"Running with normalization = {norm}, rarefaction = {rare}")
+        result = run_nested_cv_experiment_avg(datasets, normalization=norm, rarefaction=rare)
+        filename = f"NestedRare{rare}{norm}Results.csv"
+        result.to_csv(filename)
